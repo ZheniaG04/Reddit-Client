7b68ccd851b9639410a84e113e29a12f8c8463ff
Yevhenii Huts <evhenguts@gmail.com>
2021-05-30 17:46:53 +0300

    Modified imageSaver

--- Reddit Client/Model/ImageSaver.swift
+++ Reddit Client/Model/ImageSaver.swift
@@ -12,12 +12,8 @@ class ImageSaver: NSObject {
         if let url = url, let safeURL = URL(string: url),
            let data = try? Data(contentsOf: safeURL),
            let image = UIImage(data: data) {
-            writeToPhotoAlbum(image: image)
+            UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
         }
-    }
-    
-    private func writeToPhotoAlbum(image: UIImage) {
-        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
     }
 
     @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
