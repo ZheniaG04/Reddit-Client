b3f9f239b7564e80293dc9b280f2d301511b69df
Yevhenii Huts <evhenguts@gmail.com>
2021-05-30 17:14:20 +0300

    Added networkManager test

--- Reddit ClientTests/Reddit_ClientTests.swift
+++ Reddit ClientTests/Reddit_ClientTests.swift
@@ -9,25 +9,36 @@ import XCTest
 @testable import Reddit_Client
 
 class Reddit_ClientTests: XCTestCase {
+    
+    var networkManager: NetworkManager!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        networkManager = NetworkManager()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        networkManager = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testNetworkManagerFetchData() throws {
+        
+        //Given
+        var successFetch = false
+        let fetchDataExpectation = expectation(description: "Expectation in " + #function)
+        
+        //When
+        networkManager.fetchData { result in
+            if case .success = result {
+                successFetch = true
+            } else {
+                successFetch = false
+            }
+            fetchDataExpectation.fulfill()
+        }
+        
+        //Then
+        waitForExpectations(timeout: 5.0) { _ in
+            XCTAssertTrue(successFetch)
         }
     }
-
 }
